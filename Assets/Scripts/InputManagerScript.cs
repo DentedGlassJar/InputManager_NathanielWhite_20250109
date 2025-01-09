77aa37f335c8401b21931d17a80ddb9a89c61b88
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-01-09 15:20:43 -0400

    Jumping Input Implemented

--- Assets/Scripts/InputManagerScript.cs
+++ Assets/Scripts/InputManagerScript.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputManagerScript : MonoBehaviour
+public class InputManagerScript : MonoBehaviour, GameInput.IGameplayActions
 {
-    // Update is called once per frame
-    void Update()
+    public GameInput gameInput;
+    private Action jumpEvent;
+
+    // --METHODS--
+
+
+    private void Start()
     {
-        if(Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            Debug.Log("SpaceKey was pressed!");
-        }
+        gameInput = new GameInput();
+
+        gameInput.Gameplay.Enable();
+        gameInput.Gameplay.SetCallbacks(this);
     }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            Debug.Log("Jump Performed");
+            jumpEvent?.Invoke();
+        }    
+    }
+
 }
